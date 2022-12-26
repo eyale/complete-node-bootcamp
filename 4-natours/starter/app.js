2de40d45cd1f9f2250bac2442db4dfd39b5c6aed
anton <4eyalush@gmail.com>
2022-12-26 23:20:19 +0200

    REST: get list of items, get one item, add one item

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,21 +1,71 @@
+const fs = require('fs');
 const express = require('express');
+
+const K = require(`${__dirname}/misc/constants`);
 const app = express();
 
-const K = {
-  APP_NAME: 'Natours',
-  PORT: 3000,
-};
+// middleware
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).json({ app: K.APP_NAME, message: 'HELLO from 🔙 🔚' });
+const tourFilePath = `${__dirname}/dev-data/data/tours-simple.json`;
+const toursJSONData = JSON.parse(fs.readFileSync(tourFilePath));
+
+// Route handler - callback function that handle route request
+const Routes = {
+  v1: {
+    tours: `/api/${K.versions.v1}/tours`,
+    tourById: `/api/${K.versions.v1}/tours/:id`,
+  },
+};
+app.post(Routes.v1.tours, (req, res) => {
+  const newTourItemId = toursJSONData[toursJSONData.length - 1].id + 1;
+  const newTourItem = Object.assign({ id: newTourItemId }, req.body);
+  console.log('❗ > newTourItem', newTourItem);
+
+  toursJSONData.push(newTourItem);
+  const jsonString = JSON.stringify(toursJSONData);
+  fs.writeFile(tourFilePath, jsonString, (err) => {
+    console.log('❗ > err', err);
+
+    res.status(201).json({
+      status: K.status.success,
+      data: {
+        tour: newTourItem,
+      },
+    });
+  });
 });
 
-app.post('/', (req, res) => {
-  res
-    .status(200)
-    .json({ app: K.APP_NAME, message: 'POST method is also works properly' });
+app.get(Routes.v1.tours, (req, res) => {
+  res.status(200).json({
+    status: K.status.success,
+    count: toursJSONData.length,
+    data: {
+      tours: toursJSONData,
+    },
+  });
+});
+
+app.get(Routes.v1.tourById, (req, res) => {
+  const id = parseInt(req.params.id);
+  const tourItem = toursJSONData.find((tour) => tour.id === id);
+
+  // if (id > toursJSONData.length) {
+  if (!tourItem) {
+    return res.status(404).json({
+      status: K.status.fail,
+      message: `Invalid id: ${id}`,
+    });
+  }
+
+  res.status(200).json({
+    status: K.status.success,
+    data: {
+      tour: toursJSONData.find((tour) => tour.id === id),
+    },
+  });
 });
 
 app.listen(K.PORT, () => {
-  console.log(`App running at ${K.PORT}...`);
+  console.log(`${K.APP_NAME} is 🏃🏼‍♂️ at ${K.PORT}...`);
 });
---
+++ 4-natours/starter/misc/constants.js
@@ -0,0 +1,13 @@
+const K = {
+  APP_NAME: 'Natours',
+  PORT: 3000,
+  versions: {
+    v1: 'v1',
+  },
+  status: {
+    success: 'success',
+    fail: 'fail',
+  },
+};
+
+module.exports = K;
