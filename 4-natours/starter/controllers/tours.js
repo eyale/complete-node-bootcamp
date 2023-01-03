d6b7767e28ead4d5d36474516ee529189984fda1
anton <4eyalush@gmail.com>
2023-01-03 09:18:35 +0200

    get all and get by id

--- 4-natours/starter/controllers/tours.js
+++ 4-natours/starter/controllers/tours.js
@@ -1,29 +1,46 @@
-const fs = require('fs');
+/**
+ *
+ * CONTROLLER Tours
+ */
 
 const K = require(`${__dirname}/../misc/constants.js`);
 const Tour = require(`${__dirname}/../models/tour.js`);
 
-const onGetAll = (req, res) => {
-  res.status(200).json({
-    status: K.STATUS.success,
-    requestedAt: req.requestedAt
-    // count: K.toursData.length,
-    // data: {
-    //   tours: K.toursData
-    // }
-  });
+const onGetAll = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: K.STATUS.success,
+      requestedAt: req.requestedAt,
+      count: tours.length,
+      data: {
+        tours: tours
+      }
+    });
+  } catch (error) {
+    res.status(401).json({
+      status: K.STATUS.fail,
+      message: 'Error'
+    });
+  }
 };
 
-const onGet = (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  // const tour = K.toursData.find(item => item.id === id);
-
-  // res.status(200).json({
-  //   status: K.STATUS.success,
-  //   data: {
-  //     tour
-  //   }
-  // });
+const onGet = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const tour = await Tour.findById(id);
+    res.status(200).json({
+      status: K.STATUS.success,
+      data: {
+        tour
+      }
+    });
+  } catch (error) {
+    res.status(401).json({
+      status: K.STATUS.fail,
+      message: 'Error'
+    });
+  }
 };
 
 const onAddNew = async (req, res) => {
