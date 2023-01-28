b95b85a7c94e00dd31bdb9cf597cf649b6f4ae69
anton <4eyalush@gmail.com>
2023-01-28 22:23:37 +0200

    generate jwt

--- 4-natours/starter/controllers/auth.js
+++ 4-natours/starter/controllers/auth.js
@@ -3,17 +3,29 @@
  * User controller
  */
 
+const jwt = require('jsonwebtoken');
+
 const K = require(`${__dirname}/../misc/constants`);
 const H = require(`${__dirname}/../misc/helpers`);
 
 const User = require('../models/user');
 
 const signupAsync = H.catchAsync(async (req, res, next) => {
-  // const newUser = await User.save(req.body);
-  const newUser = await User.create(req.body);
+  const { name, email, password, passwordConfirm } = req.body;
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+    passwordConfirm
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
 
   res.status(200).json({
     status: K.STATUS.success,
+    token,
     data: { user: newUser }
   });
 });
