3445c34ea97c9e3f492b7f0c3e6a73c8f60e5d90
anton <4eyalush@gmail.com>
2023-02-09 14:00:56 +0200

    update ratingsAverage and ratingsQuantity after add new review

--- 4-natours/starter/models/review.js
+++ 4-natours/starter/models/review.js
@@ -4,6 +4,8 @@
 
 const mongoose = require('mongoose');
 
+const Tour = require(`${__dirname}/tour`);
+
 const reviewSchema = new mongoose.Schema(
   {
     review: {
@@ -43,10 +45,10 @@ reviewSchema.pre(/^find/, function(next) {
 });
 
 reviewSchema.pre(/^find/, function(next) {
-  const tourOptions = {
-    path: 'tour',
-    select: 'name'
-  };
+  // const tourOptions = {
+  //   path: 'tour',
+  //   select: 'name'
+  // };
   const userOptions = {
     path: 'user',
     select: 'name photo'
@@ -54,6 +56,44 @@ reviewSchema.pre(/^find/, function(next) {
   // this.populate(tourOptions).populate(userOptions);
   this.populate(userOptions);
   next();
+});
+
+/**
+ *
+ * @param {ratingsQuantity} and @param {ratingsAverage}
+ * for Tour item
+ * after adding new review
+ */
+reviewSchema.statics.calcAverageRating = async function(tourId) {
+  // 1 - pipeline options
+  const matchOpt = {
+    $match: {
+      tour: tourId
+    }
+  };
+  const statsOpt = {
+    $group: {
+      _id: '$tour',
+      nRating: {
+        $sum: 1
+      },
+      avgRating: {
+        $avg: '$rating'
+      }
+    }
+  };
+  // 2 - running pipeline
+  const stats = await this.aggregate([matchOpt, statsOpt]);
+
+  // 3 - update Tour
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating
+  });
+};
+
+reviewSchema.post('save', function() {
+  this.constructor.calcAverageRating(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
