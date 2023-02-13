e52d11667ddf8dd1be991087b8dad1c0d4bfc24a
anton <4eyalush@gmail.com>
2023-02-13 11:21:05 +0200

    loop tour/card itemin pug file

--- 4-natours/starter/controllers/views.js
+++ 4-natours/starter/controllers/views.js
@@ -1,10 +1,20 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tour');
+const H = require('../misc/helpers');
+
+exports.getOverview = H.catchAsync(async (req, res) => {
+  // get data from collection
+  const tours = await Tour.find();
+
+  // build template
+  // render template
   res.status(200).render('overview', {
-    title: 'The Forest Hiker Tour'
+    title: 'All Tours',
+    tours
   });
-};
+});
+
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
-    title: 'All Tours'
+    title: 'The Forest Hiker Tour'
   });
 };
